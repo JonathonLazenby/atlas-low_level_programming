82224dfaeb014cd4943a62b5210107aeabf0ded6
JonathonLazenby <lazenbyjonathon@gmail.com>
2024-01-20 21:56:48 +0000

    6c

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,8 +1,14 @@
 
 #include <stdio.h>
 
-int main(void)
-{
-	printf("Size of char: %zu byte(s)\n", sizeof(char));
-	return (0);
+int main() {
+    // Print the size of various types
+    printf("Size of char: %lu bytes\n", sizeof(char));
+    printf("Size of int: %lu bytes\n", sizeof(int));
+    printf("Size of float: %lu bytes\n", sizeof(float));
+    printf("Size of double: %lu bytes\n", sizeof(double));
+    printf("Size of long: %lu bytes\n", sizeof(long));
+    printf("Size of long long: %lu bytes\n", sizeof(long long));
+
+    return 0;
 }
