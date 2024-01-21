f548ad1a133659906a931009ad92f74878afe5f0
JonathonLazenby <JonathonLazenby>
2024-01-21 18:02:33 +0000

    5c

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,23 @@
+
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ * main - This prints  "the alphabet in lowercase"
+ * Return: returns nothing
+*/
+
+int main(void)
+{
+	int digit = 0
+
+	while (digit <= 9)
+	{
+		putchar(digit);
+		digit++;
+	}
+	putchar('\n');
+
+	return (0);
+}
+
