35f779e9fd55174a6cb8b2be3a74c88f10224d4f
JonathonLazenby <JonathonLazenby>
2024-01-21 18:39:07 +0000

    6-9c

---
+++ variables_if_else_while/7-print-telaphla.c
@@ -0,0 +1,22 @@
+
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ * main - This prints  "the alphabet in lowercase"
+ * Return: returns nothing
+*/
+
+int main(void)
+{
+        char letter = 'z';
+
+        while (letter >= 'a')
+        {
+                putchar(letter);
+                letter--;
+        }
+        putchar('\n');
+
+        return (0);
+}
---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,31 @@
+
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ * main - This prints  "the alphabet in lowercase"
+ * Return: returns nothing
+*/
+
+int main(void)
+{
+        char hexdigit = '0';
+
+        while (hexdigit <= '9')
+        {
+                putchar(hexdigit);
+                hexdigit++;
+        }
+	
+	hexdigit = 'a';
+
+	while (hexdigit <= 'f')
+	{
+		putchar(hexdigit);
+		hexdigit++;
+	}
+
+        putchar('\n');
+
+        return (0);
+}
