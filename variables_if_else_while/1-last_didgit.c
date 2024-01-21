ddb1a1fda1a442994fdff24449762e395a4cbd11
JonathonLazenby <JonathonLazenby>
2024-01-21 17:07:53 +0000

    1c

---
+++ variables_if_else_while/1-last_didgit.c
@@ -0,0 +1,32 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/**
+ * main - This prints  "positive and negative of random numbers,"
+ * Return: returns nothing
+*/
+
+int main(void)
+{
+        int n;
+
+        srand(time(0));
+        n = rand() - RAND_MAX / 2;
+        /* your code goes there */
+	d = abs(n)  % 10;
+        if (d > 5)
+        {
+                printf("Last digit of %d is %d and is greater than 5\n", n, d);
+        } else if (n == 0)
+        {
+                printf("Last digit of %d is %d and is 0\n", n, d);
+        } else
+        {
+                printf("Last digit of %d is %d and is less than 6 and not 0\n", n, d);
+        }
+
+        return (0);
+}
