255137a5bf6b16038c3a7eb2a88a9d289be5c63b
JonathonLazenby <lazenbyjonathon@gmail.com>
2024-01-26 21:41:33 +0000

    1c

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _isdigit - tells if an ascii is a digit or not
+ *
+ * @c: a digit or something.
+ *
+ * Return: 1 if digit
+ *         0 is not
+ */
+
+int _isdigit(int c)
+{
+        int 0 = 0;
+        int 9 = 9;
+
+        if (c >= A && c <= Z)
+                return (1);
+        else
+                return (0);
+
+}
