374275111cc0a8e1340398014cc35d604febfb4d
JonathonLazenby <lazenbyjonathon@gmail.com>
2024-01-26 21:51:38 +0000

    2c

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - mul two numbers
+ *
+ * @a: a number
+ * @b: another number
+ *
+ * Return: the mul of the numbers
+ *
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
