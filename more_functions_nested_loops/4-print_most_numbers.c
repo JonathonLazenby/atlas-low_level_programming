f34665c062664fd80e36e827a7eacc33491d03e8
JonathonLazenby <lazenbyjonathon@gmail.com>
2024-01-27 19:45:14 +0000

    4c

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_most_numbers = prints one through nine without two and four
+ * print_most_numbers - prints one through nine without two and four
  *
  *
  * Return: nothing
