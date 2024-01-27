a25ee9dbcb77f05bd6666f85cc7fadcdcd963fef
JonathonLazenby <lazenbyjonathon@gmail.com>
2024-01-27 19:30:27 +0000

    1c

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -1,11 +1,21 @@
 #include "main.h"
 
 /**
- * _islower - tells if an ascii is a lowercase or not
+ * _print_most_numbers
  *
  * @c: a letter or something.
  *
- * Return: 1 if lowercase
- *         0 is uppercase
+ * Return: nothing
  */
 
+void _print_most_numbers(void)
+{
+	char digit;
+	for (digit ='0'; digit <='9'; digit++)
+	{
+		if (digit !='2' && digit !='4')
+		{
+			_putchar(digit);
+		}
+	}
+}
