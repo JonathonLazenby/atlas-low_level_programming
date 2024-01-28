008d193c828fabd6e86ec6f086af546242d62de6
JonathonLazenby <lazenbyjonathon@gmail.com>
2024-01-28 17:39:24 +0000

    0c

---
+++ pointers_arrays_strings/main.h
@@ -0,0 +1,16 @@
+#ifndef main_h
+	#define main_h
+
+
+	int _putchar(char);
+	void reset_to_98(int *n);
+	void swap_int(int *a, int *b);
+	int _strlen(char *s);
+	void _puts(char *str);
+	void print_rev(char *s);
+	void rev_string(char *s);
+	void print_line(int n);
+	void print_diagonal(int n);
+	void print_square(int size);
+
+	#endif
