73d84d646b398929aed489cbc53b1f2af92eeae4
JonathonLazenby <lazenbyjonathon@gmail.com>
2024-01-28 16:10:13 +0000

    8c

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -11,15 +11,18 @@ void print_square(int size)
 	if (size > 0)
 	{
 		int i, j;
-		for (i=0; i < size; i++)
+		
+		for (i= 0; i < size; i++)
 		{
 			for (j = 0; j < size; j++)
 			{
 				_putchar('#');
 			}
 			_putchar('\n');
+		
 		}
-	}else{
+	}else
+	{
 		_putchar('\n');
 	}
 }
