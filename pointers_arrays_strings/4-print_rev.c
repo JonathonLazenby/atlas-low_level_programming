bb2e408960aa20089a5c9f5efeee763cd53000ee
JonathonLazenby <lazenbyjonathon@gmail.com>
2024-01-28 18:30:05 +0000

    4c

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -12,17 +12,16 @@ void print_rev(char *s)
 	int length;
 
 	length = 0;
-	while (s != '\0')
+	while (*s != '\0')
 	{
 		length++;
 		s++;
 	}
-
-	for (int i = length; i >= 0; i--)
+	int i;
+	
+	for (i = length; i >= 0; i--)
 	{
 		_putchar(s[i]);
 	}	
 	_putchar('\n');
 }
-
-
