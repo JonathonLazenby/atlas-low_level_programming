fec838d8654ee55139af6506b328a4a15852208b
JonathonLazenby <lazenbyjonathon@gmail.com>
2024-01-28 16:45:26 +0000

    9c

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -15,7 +15,7 @@ int main(void)
 		{
 			printf("FizzBuzz ");
 
-		} 
+		}
 		else if (i % 3 == 0)
 		{
 			printf("Fizz ");
@@ -31,7 +31,7 @@ int main(void)
 			printf("%d ", i);
 		}
 	}
-	
-	printf("\n");
+
+	printf ("\n");
 	return(0);
 }
