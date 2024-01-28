3b36035894f699b5b05e5b8d3f8329842da3e730
JonathonLazenby <lazenbyjonathon@gmail.com>
2024-01-28 16:01:13 +0000

    8c

--- more_functions_nested_loops/8-print_sqaure.c
+++ more_functions_nested_loops/8-print_sqaure.c
@@ -1,12 +1,24 @@
 #include "main.h"
 
 /**
- * _islower - tells if an ascii is a lowercase or not
+ * print_sqaure - prints a sqaure line in the terminal
  *
- * @c: a letter or something.
- *
- * Return: 1 if lowercase
- *         0 is uppercase
+ * Return: nothing
  */
 
-
+void print_sqaure(int size)
+{
+	if (n > 0)
+	{
+		int i, j;
+		for (i=0; i < size; i++)
+		{
+			for (j = 0; j < size; j++)
+			{
+				_putchar('#');
+			}
+		}
+	else{
+		_putchar('\n');
+	}
+}
