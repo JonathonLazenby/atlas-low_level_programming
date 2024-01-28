2c7ca0b3b81af60e37d10f2ef1bfc8345b6cec38
JonathonLazenby <lazenbyjonathon@gmail.com>
2024-01-28 17:51:15 +0000

    1c

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * swap_int - swaps integer values
+ *
+ *@b: a pointer
+ * @a: a pointer
+ *
+ */
+
+void swap_int(int *a, int *b)
+
+{
+	int temp = *a;
+	*a = *b;
+	*b = temp
+}
