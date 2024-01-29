5fdba7c1fe79803dfbe8d75f9111f5c0f1d6520e
JonathonLazenby <lazenbyjonathon@gmail.com>
2024-01-29 20:07:04 +0000

    7c

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -3,24 +3,26 @@
 /**
  * print_diagonal - prints a diagonal line in the terminal
  *
+ *@n: how long diagonal line is
+ *
  * Return: nothing
  */
 
 void print_diagonal(int n)
 {
-        if (n > 0)
-        {
-                int i, j;
+	if (n > 0)
+	{
+		int i, j;
 		for (i=0; i < n; i++)
 		{
-		       	for (j = 0; j < i; j++)
-		       	{	
-                       		 _putchar(' ');
-                	}
+			for (j = 0; j < i; j++)
+			{	
+				_putchar(' ');
+			}
 			_putchar('\\');
 			_putchar('\n');
 		}
-        }else{
+	}else{
 		_putchar('\n');
 	}
 }
