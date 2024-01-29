9f4ab8cd2f82881c6993149c58ffa9c6f8b963ae
JonathonLazenby <lazenbyjonathon@gmail.com>
2024-01-29 23:34:29 +0000

    6c

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -10,17 +10,14 @@
 
 void puts2(char *str)
 {
-	int count = 0;
+	int length = 0;
 
-	while (count >= 0)
+	while (str[length] != '\0')
 	{
-		if (str[count] == '\0')
-		{
-			putchar('\n');
-			break;
-		}
-		if (count % 2 == 0)
-			putchar(str[count]);
-		count++;
+		
+		if (length % 2 == 0)
+			putchar(str[length]);
+		length++;
 	}
+	putchar('\n');
 }
