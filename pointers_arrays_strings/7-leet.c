5d94c617ab52f2d16e760f6e479efa8804fca757
JonathonLazenby <lazenbyjonathon@gmail.com>
2024-02-03 17:45:37 +0000

    7c

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -7,10 +7,11 @@
 char *leet(char *str)
 {
 	int length, i;
-	length = 0;
 	int nums[] = {52, 51, 48, 55, 49};
 	int lowCase[] = {97, 101, 111, 116, 108};
 	int upCase[] = {65, 69, 79, 84, 76};
+
+	length = 0;
 
 	while (*(str + length) != '\0')
 	{
