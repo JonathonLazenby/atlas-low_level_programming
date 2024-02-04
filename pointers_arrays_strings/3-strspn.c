c4fa196747830acb0dde3da3cac28eed98976513
JonathonLazenby <lazenbyjonathon@gmail.com>
2024-02-04 16:34:27 +0000

    2c

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -5,3 +5,26 @@
  */
 
 unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int length = 0;
+
+	while (*s != '\0')
+	{
+		char *accept_ptr = accept;
+
+		while (*accept_ptr != '\0')
+		{
+			if (*s == *accept_ptr)
+			{
+				length++;
+				break;
+			}
+			accept_ptr++;
+		}
+		if (*accept_ptr == '\0')
+			break;
+		s++;
+	}
+	return length;
+
+}
