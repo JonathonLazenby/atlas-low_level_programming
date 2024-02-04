09876b12f554c5d1d3364eb627ed35851296e02e
JonathonLazenby <lazenbyjonathon@gmail.com>
2024-02-04 17:15:32 +0000

    7c

---
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * swap_int - swaps integer values
+ *
+ *@b: a pointer
+ * @a: a pointer
+ *
+ */
+
+void print_diagsums(int *a, int size);
+{
+	int main_diagSum = 0;
+	int anti_diagSum = 0;
+	int i;
+
+	for ( i = 0; i < size; i++)
+	{
+		main_diagSum += *(a + i * size + i);
+		anti_diagSum += *(a + i * size + (size - 1 - i));
+	}
+	printf(main_diagSum);
+	printf(anti_diagSum);
+}
