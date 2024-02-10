6f61ebcdbafb16ce8093e9202f27bd6d442255a3
jonathonLazenby <lazenbyjonathon@gmail.com>
2024-02-10 23:15:59 +0000

    0c

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _memset - concatenates two strings,
+ *
+ *@s: memory
+ *
+ *@b: byte
+ *
+ *@n: bytes entred
+ * Return: to dest
+ */
+
+int main()
+{
+	printf( __FILE__);
+	return (0);
+}
+~       
