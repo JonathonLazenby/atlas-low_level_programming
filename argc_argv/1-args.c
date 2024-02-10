c1f4a4da30f8954034d4771f087dbc8669d3aed7
jonathonLazenby <lazenbyjonathon@gmail.com>
2024-02-10 23:37:03 +0000

    0c

---
+++ argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _memset - concatenates two strings,
+ *
+ *@s: memory
+ *
+ *@b: byte
+ *
+ *@n: bytes entred
+ * Return: to dest
+ */
+
+int main( int argc, char *argv[])
+{
+	printf("%d\n", argc -1);
+	return (0);
+}
