a336d965b05d140e62b64ae39749d095e9c01fc2
jonathonLazenby <lazenbyjonathon@gmail.com>
2024-02-10 23:57:57 +0000

    3c

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -22,7 +22,7 @@ int main(int argc, char *argv[])
 	int num1 = atoi(argv[1]);
 	int num2 = atoi(argv[2]);
 
-	in result = num1 * num2;
+	int result = num1 * num2;
 	printf("%d\n", result);
 	return (0);
 }
