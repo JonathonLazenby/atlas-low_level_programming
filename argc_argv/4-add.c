901aa78855d5ad162271a957e5b3ebdf3457a459
JonathonLazenby <lazenbyjonathon@gmail.com>
2024-02-12 23:19:41 +0000

    3c

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -23,11 +23,11 @@ int main(int argc, char *argv[])
 	}
 	sum = 0;
 
-	for ( i = 1; i < argc; ++i)
+	for ( i = 1; i < argc; ++i )
 	{
 		for (j = 0; argv[i][j] != '\0'; ++j)
 		{
-			if (!isdigit (argv[i][j]))
+			if(!isdigit (argv[i][j]))
 			{
 				printf("Error\n");
 				return (1);
