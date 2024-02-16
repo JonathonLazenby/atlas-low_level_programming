1745d6d510cfa7dc55b995f95ad6177ed1efaa26
JonathonLazenby <lazenbyjonathon@gmail.com>
2024-02-16 01:47:21 +0000

    2c

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -18,7 +18,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 
-	t_size = nmemb * size
+	t_size = nmemb * size;
 
 	ptr = calloc(nmemb, size);
 
