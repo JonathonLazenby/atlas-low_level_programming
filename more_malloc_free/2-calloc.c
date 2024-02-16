eef653c33b4e141e593884922a22b51d5e8e5511
JonathonLazenby <lazenbyjonathon@gmail.com>
2024-02-16 01:45:53 +0000

    1c

---
+++ more_malloc_free/2-calloc.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * print_alphabet - prints the alphabet, in lowercase,
+ * followed by a newline using only _putchar.
+ *
+ * Return: Always 0
+ *
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	size_t t_size;
+	void *ptr;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	t_size = nmeb * size
+
+	ptr = calloc(nmeb, size);
+
+	if (ptr == NULL)
+		return (NULL);
+
+	return (ptr);
+}
