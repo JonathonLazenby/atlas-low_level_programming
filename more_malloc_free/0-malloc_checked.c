bb55e793cdaa94c3533e30f25a0ca49a92350448
JonathonLazenby <lazenbyjonathon@gmail.com>
2024-02-16 01:20:58 +0000

    0c

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * print_alphabet - prints the alphabet, in lowercase,
+ * followed by a newline using only _putchar.
+ *
+ * Return: Always 0
+ *
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *ptr
+	
+	ptr = malloc(b);
+
+	if (ptr == NULL)
+		exit(98);
+
+	return(ptr);
+}
