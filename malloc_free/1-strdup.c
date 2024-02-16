91b48425a9c69d77368edf33bf179457d2c81993
JonathonLazenby <lazenbyjonathon@gmail.com>
2024-02-16 00:08:31 +0000

    0c

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+/**
+ * print_alphabet - prints the alphabet, in lowercase,
+ * followed by a newline using only _putchar.
+ *
+ * Return: Always 0
+ *
+ */
+
+char *_strdup(char *str)
+{
+	size_t length;
+	char *duplicate;
+
+	if (str == NULL)
+		return (NULL);
+	length = strlen(str);
+	
+	duplicate = (char*)malloc((length +1)* sizeof(char));
+
+	if (duplicate == NULL)
+		return (NULL);
+
+	strcpy(duplicate, str);
+
+	return (duplicate);
+}
