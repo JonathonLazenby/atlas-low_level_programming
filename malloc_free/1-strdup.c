5484f14fe52c09b41892c10d7ae833856f2efdd0
JonathonLazenby <lazenbyjonathon@gmail.com>
2024-02-20 16:37:52 +0000

    1c

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -1,6 +1,58 @@
 #include "main.h"
 #include <stdlib.h>
 #include <string.h>
+#include "main.h"
+
+/**
+ * _strlen - Returns length of a string
+ *
+ *@s: a pointer
+ *
+ *Return: length of string
+ *
+ */
+
+int _strlen(char *s)
+{
+        int length;
+
+        length = 0;
+        while (*s != '\0')
+        {
+                length++;
+                s++;
+        }
+        return (length);
+}
+
+#include<stdio.h>
+#include "main.h"
+/**
+ * _strcpy - copies the string pointed to by src,
+ *
+ * including the terminating null byte, to the
+ * buffer pointed to by dest.
+ *
+ * @dest: destination.
+ *
+ * @src: source
+ *
+ * Return: the pointer to dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int length;
+
+	for (length = 0; length >= 0; length++)
+	{
+		*(dest + length) = *(src + length);
+		if (*(src + length) == '\0')
+			break;
+	}
+	return (dest);
+}
+
 /**
  * print_alphabet - prints the alphabet, in lowercase,
  * followed by a newline using only _putchar.
@@ -16,14 +68,14 @@ char *_strdup(char *str)
 
 	if (str == NULL)
 		return (NULL);
-	length = strlen(str);
+	length = _strlen(str);
 
 	duplicate = (char*)malloc((length +1)* sizeof(char));
 
 	if (duplicate == NULL)
 		return (NULL);
 
-	strcpy(duplicate, str);
+	_strcpy(duplicate, str);
 
 	return (duplicate);
 }
