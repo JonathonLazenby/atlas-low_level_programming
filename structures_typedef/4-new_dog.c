6f06383f983a09da5f0e967dc7211eebdfa547e4
JonathonLazenby <lazenbyjonathon@gmail.com>
2024-02-23 02:03:39 +0000

    4c

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -52,8 +52,16 @@ dog_t *new_dog(char *name, float age, char *owner)
 	{
 		newdog->name = my_strdup(name);
 		newdog->owner = my_strdup(owner);
+		
+		if (newdog->name == NULL || newdog->owner == NULL)
+		{
+			free(newdog->name);
+			free(newdog->owner);
+			free(newdog);
+			return (NULL); 
+		}
 		newdog->age = age;
 	}
 	return (newdog);
-	
+		
 }
