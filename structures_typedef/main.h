bacedddf5f6c5498ba17b0439cdb2e698d84afa8
JonathonLazenby <lazenbyjonathon@gmail.com>
2024-02-23 00:30:24 +0000

    1c

--- structures_typedef/main.h
+++ structures_typedef/main.h
@@ -2,7 +2,5 @@
 #define HEADER
 
 	void init_dog(struct dog *d, char *name, float age, char *owner);
-	void print_dog(struct dog *d);
-	dog_t *new_dog(char *name, float age, char *owner);
-	void free_dog(dog_t *d);
+
 #endif
