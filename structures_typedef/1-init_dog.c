a623651a643de0d26c66b029df43806186bb27ca
JonathonLazenby <lazenbyjonathon@gmail.com>
2024-02-23 00:29:26 +0000

    main.h

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print a to z
+ *
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age
+	d->owner = owner;
+}
