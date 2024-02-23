f6acf2550ac90f873aebb813da702b6676147c67
JonathonLazenby <lazenbyjonathon@gmail.com>
2024-02-23 00:20:08 +0000

    dog.h

---
+++ structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * malloc_checked- allocates memory using malloc
+ *
+ *@b: data
+ *
+ * Return: ptr
+ */
+
+struct dog {
+	char *name;
+	float age;
+	char *owner;
+};
