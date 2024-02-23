a5a56805868f0864327d37eea4f1bc3cde299828
JonathonLazenby <lazenbyjonathon@gmail.com>
2024-02-23 00:37:29 +0000

    1c

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -5,6 +5,15 @@
  *
  */
 
+struct dog;
+void init_dog(struct dog *d, char *name, float age, char *owner)
+
+struct dog {
+	char *name;
+	float age;
+	char *owner;
+};
+
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	d->name = name;
