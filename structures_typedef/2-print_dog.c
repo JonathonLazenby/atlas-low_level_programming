4f28c75266b32d1de8e1e93efd6403d8905ca158
JonathonLazenby <lazenbyjonathon@gmail.com>
2024-02-23 00:56:15 +0000

    2c

---
+++ structures_typedef/2-print_dog.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_alphabet - print a to z
+ *
+ */
+
+ void print_dog(struct dog *d)
+{
+	if (d != NULL)
+	{
+		printf(d->name ? d->name : "nil");
+		printf(d->age);
+		printf(d->owner ? d->owner : "nil");
+	}
+}
