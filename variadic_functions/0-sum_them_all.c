561ec55c4d2fbb707632882bbe353f907673d551
JonathonLazenby <lazenbyjonathon@gmail.com>
2024-02-25 02:42:54 +0000

    0c

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -12,7 +12,9 @@
 
 int sum_them_all(const unsigned int n, ...)
 {
-	int i, sum;
+	int sum;
+	unsigned int i;
+	va_list args;
 
 	if (n == 0)
 	{
@@ -20,7 +22,6 @@ int sum_them_all(const unsigned int n, ...)
 	}
 	sum = 0;
 
-	va_list args;
 	va_start(args, n);
 
 	for (i = 0; i < n; i++)
