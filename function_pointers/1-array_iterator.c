c706b0885fa71ec006320603698ebed26fc30120
JonathonLazenby <lazenbyjonathon@gmail.com>
2024-02-25 01:20:00 +0000

    0c

---
+++ function_pointers/1-array_iterator.c
@@ -0,0 +1,24 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * print_alphabet - prints the alphabet, in lowercase,
+ * followed by a newline using only _putchar.
+ *
+ * Return: Always 0
+ *
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+
+	for (i = 0; i < size; i++)
+	{
+		if (action != NULL)
+		{
+			action(array[i]);
+		}
+	}
+}
