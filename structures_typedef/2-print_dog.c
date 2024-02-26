ea95a71f268c143a434a18ab4e4330dd8022e167
JonathonLazenby <lazenbyjonathon@gmail.com>
2024-02-26 22:19:11 +0000

    1c

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -3,16 +3,17 @@
 #include "dog.h"
 
 /**
- * print_alphabet - print a to z
+ * print_dog - prints a struct dog
  *
+ * @d: a pointer to print
  */
 
- void print_dog(struct dog *d)
+void print_dog(struct dog *d)
 {
 	if (d != NULL)
 	{
-		printf("Name: %s\n",d->name ? d->name : "(nil)");
-		printf("Age: %f\n",d->age);
-		printf("Owner: %s\n",d->owner ? d->owner : "(nil)");
+		printf("Name: %s\n", d->name ? d->name : "(nil)");
+		printf("Age: %f\n", d->age);
+		printf("Owner: %s\n", d->owner ? d->owner : "(nil)");
 	}
 }
