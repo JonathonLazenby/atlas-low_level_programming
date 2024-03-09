33dc715ac142eb76cd2cdaf0399f12c672abd60f
JonathonLazenby <lazenbyjonathon@gmail.com>
2024-03-09 23:09:16 +0000

    setup

---
+++ bit_manipulation/0-binary_to_unit.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_dog - prints a struct dog
+ *
+ * @d: a pointer to print
+ */
+
+#include <unistd.h>
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
