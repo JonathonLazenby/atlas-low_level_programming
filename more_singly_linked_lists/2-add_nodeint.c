b821e73885b3dd69ae1a5ff91adcb9589d767cb1
JonathonLazenby <lazenbyjonathon@gmail.com>
2024-03-10 15:57:06 +0000

    2c

--- more_singly_linked_lists/2-add_nodeint.c
+++ more_singly_linked_lists/2-add_nodeint.c
@@ -9,9 +9,15 @@
 
 #include <unistd.h>
 
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
-
 listint_t *add_nodeint(listint_t **head, const int n)
+{
+	list_t *new_node = (list_t *)malloc(sizeof(list_t));
+
+	if (new_node == NULL)
+		return(NULL);
+
+	new_node -> n = n;
+	new_node -> next = *head;
+	*head = new_node;
+	return(new_node);
+}
