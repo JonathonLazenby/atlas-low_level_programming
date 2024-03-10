759162ad425c9c4f4b8ec6e9b73d6c59f7a6bc74
JonathonLazenby <lazenbyjonathon@gmail.com>
2024-03-10 16:45:13 +0000

    8c

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -1,17 +1,40 @@
 #include <stdio.h>
 #include "lists.h"
-
+#include <stdlib.h>
 /**
  * print_dog - prints a struct dog
  *
  * @d: a pointer to print
  */
 
-#include <unistd.h>
-
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
-
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	listint_t *new_node = (listint_t *)malloc(sizeof(listint_t));
+	listint_t *current;
+	unsigned int i;
+
+	if (new_node == NULL)
+	{
+		return(NULL);
+	}
+	new_node->n = n;
+	if (idx == 0)
+	{
+		new_node->next = *head;
+		*head = new_node;
+		return(new_node);
+	}
+	current = *head;
+	for (i = 0; i < idx - 1 && current != NULL; i++)
+	{
+		current = current->next;
+	}
+	if (current == NULL)
+	{
+		free(new_node);
+		return(NULL);
+	}
+	new_node->next = current->next;
+	current->next = new_node;
+	return(new_node);
+}
