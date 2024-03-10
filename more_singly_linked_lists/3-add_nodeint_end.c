29f7b4c9e9743b3753060b8fbdb3a2781cad2e21
JonathonLazenby <lazenbyjonathon@gmail.com>
2024-03-10 16:07:23 +0000

    2c

--- more_singly_linked_lists/3-add_nodeint_end.c
+++ more_singly_linked_lists/3-add_nodeint_end.c
@@ -8,13 +8,6 @@
  * @d: a pointer to print
  */
 
-#include <unistd.h>
-
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
-
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
 	listint_t *current;
