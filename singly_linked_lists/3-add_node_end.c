c9ffc08e544f69907e204bf7ed2bbd71d5a73172
JonathonLazenby <lazenbyjonathon@gmail.com>
2024-03-10 01:55:44 +0000

    3c

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -36,7 +36,7 @@ list_t *add_node_end(list_t **head, const char *str)
 		{
 			current = current -> next;
 		}
-		current -> next = new_node
+		current -> next = new_node;
 	}
 	return(new_node);
 }
