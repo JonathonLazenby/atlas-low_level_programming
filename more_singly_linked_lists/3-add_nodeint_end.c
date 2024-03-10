a58a8c4468b84b39b3adfb30a3334b139633037c
JonathonLazenby <lazenbyjonathon@gmail.com>
2024-03-10 16:05:25 +0000

    2c

--- more_singly_linked_lists/3-add_nodeint_end.c
+++ more_singly_linked_lists/3-add_nodeint_end.c
@@ -15,3 +15,28 @@ int _putchar(char c)
 }
 
 listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+	listint_t *current;
+	
+	listint_t *new_node = (listint_t *)malloc(sizeof(listint_t));
+
+	if (new_node == NULL)
+		return(NULL);
+
+	new_node -> n = n;
+	new_node -> next = NULL;
+	if (*head == NULL)
+	{
+		*head = new_node;
+	}
+	else
+	{
+		current = *head;
+		while (current -> next != NULL)
+		{
+			current = current -> next;
+		}
+		current -> next = new_node;
+	}
+	return(new_node);
+}
