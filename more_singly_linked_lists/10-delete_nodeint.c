3f629f68dd406d0f0f0ed0a5885728c45350612b
JonathonLazenby <lazenbyjonathon@gmail.com>
2024-03-10 16:52:10 +0000

    8c

--- more_singly_linked_lists/10-delete_nodeint.c
+++ more_singly_linked_lists/10-delete_nodeint.c
@@ -7,11 +7,33 @@
  * @d: a pointer to print
  */
 
-#include <unistd.h>
-
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
-
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	unsigned int i;
+	listint_t *temp, *current;
+
+	if (*head == NULL)
+	{
+		return(-1);
+	}
+	if (index == 0)
+	{
+		temp = *head;
+		*head = (*head)->next;
+		free(temp)
+		return(1);
+	}
+	current = *head;
+	for (i = 0; i < index - 1 && current != NULL; i++)
+	{
+		current = current->next;
+	}
+	if (current == NULL || current->next == NULL)
+	{
+		return(-1);
+	}
+	temp = current->next;
+	current->next = current->next->next;
+	free(temp);
+	return(1);
+}
