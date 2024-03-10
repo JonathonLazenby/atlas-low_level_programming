b306dd22743b612385110bf9323293ad10cfdede
JonathonLazenby <lazenbyjonathon@gmail.com>
2024-03-10 16:13:36 +0000

    5c

--- more_singly_linked_lists/5-free_listint2.c
+++ more_singly_linked_lists/5-free_listint2.c
@@ -1,17 +1,21 @@
 #include <stdio.h>
 #include "lists.h"
-
+#include <stdlib.h>
 /**
  * print_dog - prints a struct dog
  *
  * @d: a pointer to print
  */
 
-#include <unistd.h>
-
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
-
 void free_listint2(listint_t **head)
+{
+	listint_t *next_node;
+
+	while (*head != NULL)
+	{
+		next_node = (*head)->next;
+		free(*head);
+		*head = next_node;
+	}
+	*head = NULL;
+}
