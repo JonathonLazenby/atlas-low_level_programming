8f57defe76cfc7c692557ff69c98b0e49a51b935
JonathonLazenby <lazenbyjonathon@gmail.com>
2024-03-10 01:46:51 +0000

    2c

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string,h>
 #include "lists.h"
 
 /**
@@ -9,9 +11,20 @@
 
 #include <unistd.h>
 
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
-
 list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new_node = (list_t *)malloc(sizeof(list_t));
+
+	if (new_node == NULL)
+		return(NULL);
+
+	new_node -> str = strdup(str)
+	if (new_node -> str == NULL)
+	{
+		free(new_node);
+		return(NULL);
+	}
+	new_node -> next = *head;
+	*head = new_node;
+	return(new_node);
+}
