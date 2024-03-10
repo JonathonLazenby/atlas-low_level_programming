1b885053479e63290bb742207696795d9c6e4ef6
JonathonLazenby <lazenbyjonathon@gmail.com>
2024-03-10 16:53:14 +0000

    8c

--- more_singly_linked_lists/10-delete_nodeint.c
+++ more_singly_linked_lists/10-delete_nodeint.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include "lists.h"
-
+#include <stdlib.h>
 /**
  * print_dog - prints a struct dog
  *
@@ -20,7 +20,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	{
 		temp = *head;
 		*head = (*head)->next;
-		free(temp)
+		free(temp);
 		return(1);
 	}
 	current = *head;
