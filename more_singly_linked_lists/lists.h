c5d9374f1d3f298ffc786312bd00946539533bf2
JonathonLazenby <lazenbyjonathon@gmail.com>
2024-03-10 15:48:30 +0000

    0c

--- more_singly_linked_lists/lists.h
+++ more_singly_linked_lists/lists.h
@@ -1,6 +1,20 @@
 #ifndef list
 #define list
 
+/**
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ * 
+ */
+	typedef struct listint_s
+	{
+    		int n;
+    		struct listint_s *next;
+	} listint_t;
+
 	int _putchar(char);
 	size_t listint_len(const listint_t *h);
 	listint_t *add_nodeint(listint_t **head, const int n);
