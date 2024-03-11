428cae816721b8aebdd75fa265618d4bd9ce08d9
JonathonLazenby <lazenbyjonathon@gmail.com>
2024-03-11 20:32:43 +0000

    1c

--- bit_manipulation/2-get_bit.c
+++ bit_manipulation/2-get_bit.c
@@ -8,3 +8,18 @@
  */
 
 int get_bit(unsigned long int n, unsigned int index)
+{
+	int i, size;
+	unsigned long int mask;
+
+	size = sizeof(unsigned long int) * 8 - 1;
+	for (i = size; i >= 0; i--)
+	{
+		mask = 1UL << i;
+		if (n & mask)
+			_putchar('1');
+		else
+			_putchar('0');
+	}
+	_putchar('\n');
+}
