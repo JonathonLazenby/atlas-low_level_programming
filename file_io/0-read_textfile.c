b7c42b5d9885366ebe4b21ae7b5a3c90be20b34d
JonathonLazenby <lazenbyjonathon@gmail.com>
2024-03-11 20:52:29 +0000

    0c

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include "main.h"
 #include <stdlib.h>
+#include < unistd.h>
 
 /**
  * print_dog - prints a struct dog
@@ -9,4 +10,41 @@
  */
 
 ssize_t read_textfile(const char *filename, size_t letters)
+{
+	FILE *file;
+	char *buffer;
+	ssize_t bytesread, byteswritten)
 
+	if (filename == NULL)
+		return(0);
+	
+	file = fopen(filename, "r")
+	
+	if (file == NULL)
+		return(0);
+
+	buffer = malloc(letters);
+
+	if (buffer == NULL)
+	{
+		fclose(file);
+		return(0);
+	}
+	bytesread = fread(buffer, 1, letters, file);
+
+	if (bytesread == -1 || bytesread == 0)
+	{
+		free(buffer);
+		fclose(file);
+		return(0);
+	}
+	byteswritten = write(STDOUT_FILENO, BUFFER, bytesread)
+	free(buffer);
+	fclose(file);
+
+	If (byteswritten == -1 || (size_t) byteswritten != bytesread)
+	{
+		return(0);
+	}
+	return(bytesread);
+}
