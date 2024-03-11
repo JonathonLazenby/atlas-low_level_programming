00ca2c02ad914c0f2398e886f7e17d93341c34f2
JonathonLazenby <lazenbyjonathon@gmail.com>
2024-03-11 23:05:19 +0000

    betty

--- more_singly_linked_lists/2-add_nodeint.c
+++ more_singly_linked_lists/2-add_nodeint.c
@@ -2,9 +2,13 @@
 #include "lists.h"
 #include <stdlib.h>
 /**
- * print_dog - prints a struct dog
+ * add_nodeint - adds new node at beginning of said linked list
  *
- * @d: a pointer to print
+ * @head: 1st Element
+ *
+ * @n:thing being added
+ *
+ * Return: the address of changed element
  */
 
 #include <unistd.h>
@@ -14,10 +18,10 @@ listint_t *add_nodeint(listint_t **head, const int n)
 	listint_t *new_node = (listint_t *)malloc(sizeof(listint_t));
 
 	if (new_node == NULL)
-		return(NULL);
+		return (NULL);
 
-	new_node -> n = n;
-	new_node -> next = *head;
+	new_node->n = n;
+	new_node->next = *head;
 	*head = new_node;
-	return(new_node);
+	return (new_node);
 }
