63bcfb2053837418ab3120e7fbdb18cbb17508ac
JonathonLazenby <lazenbyjonathon@gmail.com>
2024-03-11 20:58:50 +0000

    0c

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -18,7 +18,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (filename == NULL)
 		return(0);
 	
-	file = fopen(filename, "r");
+	file = open(filename, "r");
 	
 	if (file == NULL)
 		return(0);
