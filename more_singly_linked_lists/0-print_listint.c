2cf49679f4e737f61513d112f9cfd35b1402e33f
JonathonLazenby <lazenbyjonathon@gmail.com>
2024-03-11 22:44:45 +0000

    betty

--- more_singly_linked_lists/0-print_listint.c
+++ more_singly_linked_lists/0-print_listint.c
@@ -2,9 +2,11 @@
 #include "lists.h"
 
 /**
- * print_dog - prints a struct dog
+ * print_listint - prints all elements of a list
  *
- * @d: a pointer to print
+ * @h: 1st element
+ *
+ * Return: elements in list
  */
 
 size_t print_listint(const listint_t *h)
@@ -12,13 +14,12 @@ size_t print_listint(const listint_t *h)
 	size_t node_count;
 
 	node_count = 0;
-	
+
 	while (h != NULL)
 	{
-		printf("%d\n", (h -> n));
+		printf("%d\n", (h->n));
 		node_count++;
-		h = h -> next;
+		h = h->next;
 	}
-
-	return(node_count);
+	return (node_count);
 }
