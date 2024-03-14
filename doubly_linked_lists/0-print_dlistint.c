13063fe0235f08d7b582e83180922752851d3e50
JonathonLazenby <lazenbyjonathon@gmail.com>
2024-03-14 19:57:53 +0000

    set up

---
+++ doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * list_len - # of elements in said linked list
+ *
+ * @h: checked list
+ *
+ * Return: # of elements
+ */
+
+size_t print_dlistint(const dlistint_t *h);
---
+++ hash_tables/0-hash_table_create.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "hash_tables.h"
+
+/**
+ * list_len - # of elements in said linked list
+ *
+ * @h: checked list
+ *
+ * Return: # of elements
+ */
+
+hash_table_t *hash_table_create(unsigned long int size)
