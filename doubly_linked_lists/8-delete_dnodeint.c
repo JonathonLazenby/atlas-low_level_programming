79746be60253c42e4d35718099930dd0a35b30c4
JonathonLazenby <lazenbyjonathon@gmail.com>
2024-03-14 22:17:20 +0000

    7c

---
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,49 @@
+#include <stdlib.h>
+#include <stddef.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * list_len - # of elements in said linked list
+ *
+ * @h: checked list
+ *
+ * Return: # of elements
+ */
+
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	dlistint_t *current = *h;
+	unsigned int i;
+
+	if (*head == NULL)
+	{
+		return (-1);
+	}
+	if (index == 0)
+	{
+		*head = (*head)->next;
+		if (*head != NULL)
+		{
+			(*head)->prev = NULL;
+		}
+		free(current);
+		return (1);
+	}
+	for (i = 0; current != NULL && i < index; i++)
+	{
+		current = current->next;
+	}
+	if (current == NULL)
+		return (-1);
+	if (current->prev != NULL)
+	{
+		current->prev->next = current->next;
+	}
+	if (current->next != NULL)
+	{
+		current->next->prev = current->prev;
+	}
+	free(current);
+	return (1);
+}
