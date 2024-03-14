9ce21608cbbb4e75e0a7722a70df320c1936716c
JonathonLazenby <lazenbyjonathon@gmail.com>
2024-03-14 23:46:24 +0000

    5c test

--- hash_tables/5-hash_table_print.c
+++ hash_tables/5-hash_table_print.c
@@ -24,9 +24,8 @@ void hash_table_print(const hash_table_t *ht)
 
 	for (i = 0; i < ht->size; i++)
 	{
-		printf("before");
 		current = ht->array[i];
-		printf("'%s'", current->key);
+		printf(ht->array[i]->value);
 		while (current != NULL)
 		{
 			printf("'%s': '%s'", current->key, current->value);
