b3eac488c496af72cac107d68f6efc09d9c0a78f
JonathonLazenby <lazenbyjonathon@gmail.com>
2024-03-14 22:00:03 +0000

    7c

---
+++ doubly_linked_lists/7-inset_dnodeint.c
@@ -0,0 +1,22 @@
+#include <stdib.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * list_len - # of elements in said linked list
+ *
+ * @h: checked list
+ *
+ * Return: # of elements
+ */
+
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *current = *h;
+	dlistint_t *new_node = malloc(sizeof(dlistint_t));
+
+	if (idx == 0)
+	{
+		return (add_dnodeint(h, n));
+	}
+}
