b67444e22f3b7cc6c30ca1809e75cecda654ff82
JonathonLazenby <lazenbyjonathon@gmail.com>
2024-03-14 23:20:49 +0000

    5c

---
+++ hash_tables/5-hash_table_print.c
@@ -0,0 +1,39 @@
+#include <stdlib.h>
+#include <stddef.h>
+#include "hash_tables.h"
+#include <string.h>
+#include <stdio.h>
+/**
+ * list_len - # of elements in said linked list
+ *
+ * @h: checked list
+ *
+ * Return: # of elements
+ */
+
+void hash_table_print(const hash_table_t *ht)
+{
+	hash_node_t *current;
+	unsigned long int i;
+	
+	if (ht == NULL)
+	{
+		return ();
+	}
+	printf("{");
+
+	for (i = 0; i < ht->size; i++)
+	{
+		current = ht->array[i];
+		while (current != NULL)
+		{
+			printf("'%s': '%s'", current->key, current->value);
+			if (current->next != NULL)
+			{
+				printf(", ");
+			}
+			current = current->next;
+		}
+	}
+	printf("}\n");
+}
