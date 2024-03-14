5ca7c3e92ef0b761ca20581082c581eef402f52e
JonathonLazenby <lazenbyjonathon@gmail.com>
2024-03-14 21:47:12 +0000

    5c

--- doubly_linked_lists/5-get_dnodeint.c
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -18,7 +18,7 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 	while (current != NULL && count < index)
 	{
 		current = current->next;
-		count++
+		count++;
 	}
 	return (current);
 }
