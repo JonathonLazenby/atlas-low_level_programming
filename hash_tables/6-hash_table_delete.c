964edc36ccdd091ca5ebc8f0f183efd60b6a827a
JonathonLazenby <lazenbyjonathon@gmail.com>
2024-03-14 23:36:09 +0000

    6c

--- hash_tables/6-hash_table_delete.c
+++ hash_tables/6-hash_table_delete.c
@@ -13,11 +13,11 @@
 
 void hash_table_delete(hash_table_t *ht)
 {
-	if (ht == NULL)
-		return;
-
 	unsigned long int i;
 	hash_node_t *current, *temp;
+	
+	if (ht == NULL)
+		return;
 
 	for (i = 0; i < ht->size; i++)
 	{
