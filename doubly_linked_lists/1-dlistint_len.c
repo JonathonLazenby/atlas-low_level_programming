0b9c4af920952fdd42ae63f8fccde7605e0ad31a
JonathonLazenby <lazenbyjonathon@gmail.com>
2024-03-14 21:22:36 +0000

    1c

---
+++ doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * list_len - # of elements in said linked list
+ *
+ * @h: checked list
+ *
+ * Return: # of elements
+ */
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
