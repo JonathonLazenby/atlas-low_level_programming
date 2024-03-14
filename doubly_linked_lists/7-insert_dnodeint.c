14a186fa5321f586c1a79cde59713426b81516be
JonathonLazenby <lazenbyjonathon@gmail.com>
2024-03-14 22:08:31 +0000

    7c

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -40,7 +40,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	
 	if (current->next != NULL)
 	{
-		current->next->prev = new_node:
+		current->next->prev = new_node;
 	}
 	current->next = new_node;
 	return (new_node);
