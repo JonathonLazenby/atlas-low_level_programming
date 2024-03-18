c40d50e68518909fc48ee490d2fa95fb554007af
JonathonLazenby <lazenbyjonathon@gmail.com>
2024-03-18 22:02:14 +0000

    betty style + doc

--- hash_tables/2-key_index.c
+++ hash_tables/2-key_index.c
@@ -3,15 +3,18 @@
 #include "hash_tables.h"
 
 /**
- * list_len - # of elements in said linked list
+ * key_index - gives you index of a key
  *
- * @h: checked list
+ * @key: key
  *
- * Return: # of elements
+ * @size: array size
+ *
+ * Return: index of key
  */
 
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
 	unsigned long int hash_value = hash_djb2(key);
+	
 	return (hash_value % size);
 }
