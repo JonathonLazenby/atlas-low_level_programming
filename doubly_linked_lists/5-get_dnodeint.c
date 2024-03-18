d40e9536f0eb49ffa104266fbfc622ad501782b1
JonathonLazenby <lazenbyjonathon@gmail.com>
2024-03-18 21:41:44 +0000

    betty style + doc

--- doubly_linked_lists/5-get_dnodeint.c
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -3,11 +3,13 @@
 #include "lists.h"
 
 /**
- * list_len - # of elements in said linked list
+ * get_dnodeint_at_index - returns nth node of a linked list
+ * 
+ * @head: head node
  *
- * @h: checked list
+ * @index: node to get
  *
- * Return: # of elements
+ * Return: address of certain node
  */
 
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
